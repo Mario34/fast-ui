b9926e76925e5c1ac08d895f0ebcb7b46302acd8
mario34 <1506747508@qq.com>
2020-08-12 00:10:50 +0800

    feat: 添加Layout特性

--- packages/row-col/index.tsx
+++ packages/row-col/index.tsx
@@ -7,15 +7,20 @@ const Col = defineComponent({
       type: Number,
       default: 24,
     },
+    offset: {
+      type: Number,
+      default: 0,
+    },
   },
   setup(props, ctx) {
     const { default: default_ } = ctx.slots;
-    const { col } = toRefs(props);
+    const { col, offset } = toRefs(props);
     return () => (
       <div
         class={{
           'fa-col': true,
           [`fa-col-${col.value || 24}`]: true,
+          [`fa-col-offset-${offset.value}`]: !!offset.value,
         }}
       >
         {default_ && default_()}
--- packages/row/index.tsx
+++ packages/row/index.tsx
@@ -2,10 +2,22 @@ import { defineComponent } from 'vue';
 import './index.scss';
 
 const Row = defineComponent({
+  props: {
+    space: {
+      type: Number,
+      default: 0,
+    },
+  },
   setup(props, ctx) {
     const { default: default_ } = ctx.slots;
+    const { space } = props;
     return () => (
-      <div class='fa-row'>
+      <div
+        class='fa-row'
+        style={{
+          marginBottom: space ? `${space}px` : '',
+        }}
+      >
         {default_ && default_()}
       </div>
     );
